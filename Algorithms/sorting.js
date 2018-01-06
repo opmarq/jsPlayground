2f0f7e6dc93aad01c998db5c6615b47cf0998ec7
omar chajia <opmarq@gmail.com>
2018-01-06 11:29:58 +0000

    Adding the quickSort

--- Algorithms/sorting.js
+++ Algorithms/sorting.js
@@ -120,3 +120,29 @@ function mergeSort(array)
     return merge(mergeSort(left),mergeSort(right));
 
 }
+
+function quickSort(array)
+{
+
+    if(array.length < 2)
+        return array;
+  
+    let pivot = array[array.length - 1];
+
+    let right = [];
+    let left = [];
+
+    for(let i = 0; i < array.length - 1; i++)
+    {
+        if(array[i] <= pivot)
+        {
+            left.push(array[i]);
+        }else{
+
+            right.push(array[i]);
+        }
+    }
+
+    return [...quickSort(left),pivot,...quickSort(right)];
+
+}
